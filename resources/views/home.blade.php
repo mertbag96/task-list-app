58594057fa5a1cd62df4a5b901f91afe096b8d92
Mert Bağ <bagmert1996@gmail.com>
2024-06-01 23:02:42 +0300

    Preparing homepage

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -4,6 +4,24 @@
 
 @section("content")
 
+    <section class="home">
+
+        <div class="home-content">
+            <h1 class="title">
+                Stay <span>organized</span> and manage your <span>tasks</span>.
+            </h1>
+            <p class="description">
+                Create tasks, assign tasks to users, organize users with teams.
+            </p>
+            <a href="#" class="link">Get started</a>
+        </div>
+
+        <div class="home-content">
+            <img src="{{ asset("assets/images/home.png") }}" alt="Home">
+        </div>
+
+    </section>
+
 @endsection
 
 
