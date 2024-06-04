e463626a577f0122908eb9480c53dda8c88826d2
Mert Bağ <bagmert1996@gmail.com>
2024-06-04 03:34:09 +0300

    Preparing features page

--- resources/views/website/features.blade.php
+++ resources/views/website/features.blade.php
@@ -4,6 +4,22 @@
 
 @section("content")
 
-    Features Page
+    <section class="features container">
+
+        <div class="features-content">
+
+            <h1 class="title">
+                Organize <span>teams</span>, assign <span>users</span>, and complete <span>tasks</span>.
+            </h1>
+
+        </div>
+
+        <div class="features-content">
+
+            <img src="{{ asset("assets/images/features/features.png") }}" alt="Features">
+
+        </div>
+
+    </section>
 
 @endsection
